12eca88e9aa23f529ba70a2e6b63836d80346183
alexandre <alexandrveira@gmail.com>
2018-08-20 11:52:08 -0300

    Criando operacao de instanciacao de Categoria

--- src/main/java/com/alexandre/api/ApiApplication.java
+++ src/main/java/com/alexandre/api/ApiApplication.java
@@ -1,12 +1,33 @@
 package com.alexandre.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alexandre.api.domain.Categoria;
+import com.alexandre.api.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository catRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
+
+	}
+
 }
