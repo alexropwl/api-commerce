82d33b33bb6511d169c97de8fcbfbf53834bf243
alexandre <alexandrveira@gmail.com>
2018-08-20 11:44:11 -0300

    Criando o repository e service para Categoria

---
+++ src/main/java/com/alexandre/api/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.alexandre.api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandre.api.domain.Categoria;
+import com.alexandre.api.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	CategoriaRepository categotiriaRepository;
+
+	public Categoria buscar(Integer id) {
+
+		Optional<Categoria> obj = categotiriaRepository.findById(id);
+
+		return obj.orElse(null);
+
+	}
+
+}
