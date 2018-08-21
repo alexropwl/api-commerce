53df60387936162c22b9ec518ccf954b705d6f09
alexandre <alexandrveira@gmail.com>
2018-08-21 12:19:46 -0300

    Estado e Cidade

--- src/main/java/com/alexandre/api/ApiApplication.java
+++ src/main/java/com/alexandre/api/ApiApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alexandre.api.domain.Categoria;
+import com.alexandre.api.domain.Cidade;
+import com.alexandre.api.domain.Estado;
 import com.alexandre.api.domain.Produto;
 import com.alexandre.api.repositories.CategoriaRepository;
+import com.alexandre.api.repositories.CidadeRepository;
+import com.alexandre.api.repositories.EstadoRepository;
 import com.alexandre.api.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ApiApplication implements CommandLineRunner {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	EstadoRepository estadoRepository;
+	
+	@Autowired
+	CidadeRepository cidadeRepository;
 	
 	
 
@@ -53,6 +63,40 @@ public class ApiApplication implements CommandLineRunner {
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"Sao Paulo");
+		
+		Cidade c1 = new Cidade(null,"Juiz de Fora", est1);
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+	       
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2));
+		
+		
+	     estadoRepository.saveAll(Arrays.asList(est1, est2));
+	     
+	     cidadeRepository.saveAll(Arrays.asList(c1, c2));
+	     
+	     
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
